eb6f371206e3814be9d804aea308dac4f56a7e12
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2026-02-21 20:28:34 -0300

    agrega resultadoValidacion.js

--- src/middlewares/resultadoValidacion.js
+++ src/middlewares/resultadoValidacion.js
@@ -1,15 +1,15 @@
 import { validationResult } from "express-validator"
 
+
 const resultadoValidacion = (req, res, next) => {
     const errores = validationResult(req)
     //ocurrieron errores en la validacion
     if (!errores.isEmpty()) {
-        //enviamos msj de error 400(bad request) porque falto una parte del body(producto incompleto)
-        //array de errores
+        //enviamos mensaje de error 400 (bad request)
         return res.status(400).json(errores.array())
     }
-    //continua con la siguiente ejecucion
+    //continua conla siguiente ejecucion
     next()
 }
 
-export default resultadoValidacion;
+export default resultadoValidacion; 
