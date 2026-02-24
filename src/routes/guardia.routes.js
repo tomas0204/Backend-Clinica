9c48a128db900293f4f9ce39d499367e0f1688b4
Nahuel Nuñez <nunezn53@gmail.com>
2026-02-24 09:30:33 -0300

    agrego routes para guardia

--- src/routes/guardia.routes.js
+++ src/routes/guardia.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express"
 import { crearGuardia, editarGuardiaPorID, eliminarGuardiaPorID, listarGuardia, obtenerGuardiaPorID, pruebaGuardia } from "../controllers/guardia.controllers.js"
 import validacionGuardia from "../middlewares/validacionGuardia.js"
+import validarToken from "../middlewares/validacionAuth.js"
 
 const router = Router()
 
 router.route("/testguardia").get(pruebaGuardia)
-router.route("/").post(validacionGuardia , crearGuardia).get(listarGuardia)
-router.route("/:id").get(obtenerGuardiaPorID).delete(eliminarGuardiaPorID).put(editarGuardiaPorID)
+
+router.route("/").post([validarToken ,validacionGuardia] , crearGuardia).get(listarGuardia)
+
+router.route("/:id").get(obtenerGuardiaPorID).delete([validarToken, validacionGuardia], eliminarGuardiaPorID).put([validarToken ,validacionGuardia], editarGuardiaPorID)
 
 export default router
